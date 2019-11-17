68c18d5bfcf6eb728c786ea0f64da6860c7cc603
bryan-r <56710008+bryan-r@users.noreply.github.com>
2019-11-16 21:41:50 -0800

    Initial Commit of Projects
    Commit SudokuSolver and WeatherApp projects.

---
+++ SudokuSolver.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        private static int EMPTY_CELL = 0;
+
+        public static bool SolveBoard(int [,] board)
+        {
+            return CanSolveFromCell(board, 0, 0);
+        }
+
+        private static bool CanSolveFromCell(int [,] board, int row, int col)
+        {
+            // If at end of row (col == board.GetLength()), then move to next row
+            // 0 - Row dimension of array
+            // 1 - Column dimension of array
+            if (col == board.GetLength(1))
+            {
+                col = 0;
+                row++;
+
+                if (row == board.GetLength(0))
+                {
+                    return true;
+                }
+            }
+
+            // Skip cells that already contain value
+            if (board[row, col] != EMPTY_CELL)
+            {
+                return CanSolveFromCell(board, row, col + 1);
+            }
+
+            for (int val = 1; val <= board.GetLength(0); val++)
+            {
+                int valToInsert = val;
+
+                if (CanInsertVal(board, row, col, valToInsert))
+                {
+                    board[row, col] = valToInsert;
+                    if (CanSolveFromCell(board, row, col + 1))
+                    {
+                        return true;
+                    }
+                    board[row, col] = EMPTY_CELL;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool CanInsertVal(int [,] board, int row, int col, int valToInsert)
+        {
+            //Check column of the insertion
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (valToInsert == board[i, col])
+                {
+                    return false;
+                }
+            }
+
+            //Check row of the insertion
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                if (valToInsert == board[row, j])
+                {
+                    return false;
+                }
+            }
+
+            // Check board constraints
+            // Find the size of the sub-box
+            int subBoxSize = (int)System.Math.Sqrt(board.GetLength(0));
+
+            // Find index of box
+            // Example - For 9x9 traditional sudoku index would be 0, 1, or 2
+            int verticalBoxIndex = row / subBoxSize;
+            int horizontalBoxIndex = col / subBoxSize;
+
+            // Find top left corner of sub-box
+            int topLeftSubBoxRow = subBoxSize * verticalBoxIndex;
+            int topLeftSubBoxCol = subBoxSize * horizontalBoxIndex;
+
+            //Check sub-box of the insertion
+            for (int m = 0; m < subBoxSize; m++)
+            {
+                for (int n = 0; n < subBoxSize; n++)
+                {
+                    if (valToInsert == board[topLeftSubBoxRow + m, topLeftSubBoxCol + n])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            int [ , ] sudokuBoard = new int[9, 9] {
+                {0, 3, 0, 2, 8, 7, 0, 5, 0},
+                {5, 8, 0, 6, 4, 1, 9, 0, 0},
+                {1, 0, 6, 9, 0, 0, 0, 2, 4},
+                {2, 0, 0, 0, 6, 0, 3, 0, 8},
+                {0, 9, 5, 0, 0, 0, 2, 6, 0},
+                {8, 0, 4, 0, 3, 0, 0, 0, 9},
+                {6, 2, 0, 0, 0, 5, 4, 0, 3},
+                {0, 0, 3, 8, 2, 6, 0, 1, 5},
+                {0, 5, 0, 3, 1, 4, 0, 9, 0}};
+
+            if (SolveBoard(sudokuBoard))
+            {
+                for (int i = 0; i < sudokuBoard.GetLength(0); i++)
+                {
+                    for (int j = 0; j < sudokuBoard.GetLength(1); j++)
+                    {
+                        Console.Write(string.Format("{0} ", sudokuBoard[i, j]));
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.Write("NO_SOLUTION\n");
+            }
+
+            string input = Console.ReadLine();
+        }
+    }
+}
---
+++ WeatherApp.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Xml;
+
+namespace WeatherApp
+{
+    class WeatherData
+    {
+        public WeatherData(string City)
+        {
+            city = City;
+        }
+
+        public string city;
+        private double city_temperature;
+
+        public string City { get => city; set => city = value; }
+        public double CityTemperature
+        {
+            get => city_temperature;
+            set => city_temperature = value;
+        }
+
+        public void CheckWeather()
+        {
+            // Grab temperature from API using WeatherAPI object
+            WeatherAPI DataAPI = new WeatherAPI(City);
+            CityTemperature = DataAPI.GetCityTemperature();
+        }
+    }
+
+    class WeatherAPI
+    {
+        public WeatherAPI(string city)
+        {
+            // Constructor that sets URL and retrieves XML from API
+            SetWeatherURL(city);
+            weatherXML = GetXML(weatherURL);
+        }
+
+        public double GetCityTemperature()
+        {
+            // Grab temperature value from XML document
+            XmlNode tempNode = weatherXML.SelectSingleNode("//temperature");
+            XmlAttribute tempValue = tempNode.Attributes["value"];
+            string tempString = tempValue.Value;
+            return double.Parse(tempString);
+        }
+
+        // API KEY is only avaiable to individuals who sign up for account
+        private const string APIKEY = "API KEY";
+        private string weatherURL;
+        private XmlDocument weatherXML;
+
+        private void SetWeatherURL(string city)
+        {
+            weatherURL = "http://api.openweathermap.org/data/2.5/weather/q?={city}"
+                         + "&mode=xml&units=metric&APPID={APIKEY}";
+        }
+
+        private XmlDocument GetXML(string currentURL)
+        {
+            // Dispose of WebClient once XML is retrieved
+            using (WebClient client = new WebClient())
+            {
+                string xmlContent = client.DownloadString(currentURL);
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlContent);
+                return xmlDoc;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("For which city would you like to check the weather?  ");
+            string userCity = Console.ReadLine();
+            WeatherData CityWeather = new WeatherData(userCity);
+            CityWeather.CheckWeather();
+            Console.WriteLine(CityWeather.CityTemperature);
+            string input = Console.ReadLine();
+        }
+    }
+}
